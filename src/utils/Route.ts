9451699767808ec2769edfc33c37ad4ade32254f
Quentin Derimais <quentin.derimais@gmail.com>
2026-07-16 15:57:45 +0200

    feat(errors): add 500 error page and improve routing

--- src/utils/Route.ts
+++ src/utils/Route.ts
@@ -30,9 +30,12 @@ export async function router(request: IncomingMessage, response: ServerResponse)
         if (target.middleware) {
             target.middleware(request, response)
         }
-        response.statusCode = 200;
-        const html = await readFile(join(import.meta.dirname, `../../public/${target.filePath}`), "utf8")
-        response.end(html)
+        if (target.method === "GET") {
+            get(request, response, target)
+        }
+        if (target.method === "POST") {
+            post(request, response, target);
+        }
     }
     else {
         response.statusCode = 404;
@@ -40,3 +43,19 @@ export async function router(request: IncomingMessage, response: ServerResponse)
         response.end(html)
     }
 }
+
+async function get(request: IncomingMessage, response: ServerResponse, route: Route) {
+    try {
+        const html = await readFile(join(import.meta.dirname, `../../public/${route.filePath}`), "utf8");
+        request.statusCode = 200;
+        response.end(html);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(error.message);
+            const html = await readFile(join(import.meta.dirname, `../../public/errors/500.html`), "utf8");
+            response.end(html)
+        }
+    }
+}
+
+function post(request: IncomingMessage, response: ServerResponse, route: Route) { }
